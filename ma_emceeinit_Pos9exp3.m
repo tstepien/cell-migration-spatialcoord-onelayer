1e2e3bd289814694986ffc965f9848a787ff7f55
Tracy Stepien <stepien@math.arizona.edu>
2018-03-26 15:26:21 -0700

    add files

---
+++ ma_emceeinit_Pos9exp3.m
@@ -0,0 +1,26 @@
+clc
+clear variables global
+addpath emcee_mymod
+
+% location of experimental data
+filestring = 'experimental_data/100714 Animal cap x0.8 Scion x2_0/Pos9_exp3/';
+
+%%%-------------------------- time parameters --------------------------%%%
+time.end = (120-1)*(5/60); %%% how long simulation runs in hours
+time.exp_step = 5/60; %%% how many hours between recording a curve
+
+%%%--------------------------- mesh details ----------------------------%%%
+number_nodes = 100;
+exp_px = [380,356]; %size of experimental window in pixels
+exp_scale = .177; %pixels per micron scale
+
+%%%------------------------- model parameters --------------------------%%%
+growthfunction = 'logistic_masslimited';
+
+nameofsavedfile = strcat('Pos9exp3_',growthfunction);
+
+numwalkers = 2000;
+threshold = 1500;
+
+[paramval,minquant] = myemceeinit(numwalkers,threshold,filestring,...
+    nameofsavedfile,growthfunction,time,number_nodes,exp_px,exp_scale);
