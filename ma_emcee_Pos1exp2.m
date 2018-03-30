b77ca1e715de78bb01e2af77e0666c9f1c910302
Tracy Stepien <stepien@math.arizona.edu>
2018-03-30 15:11:33 -0700

    update explants

--- ma_emcee_Pos1exp2.m
+++ ma_emcee_Pos1exp2.m
@@ -2,8 +2,10 @@ clc
 clear variables global
 addpath emcee_mymod
 
+global nameofsavedfile;
+
 % location of experimental data
-filestring = 'experimental_data/100714 Animal cap x0.8 Scion x2_0/Pos14_exp6/';
+filestring = 'experimental_data/100714 Animal cap x0.8 Scion x2_0/Pos1_exp2/';
 
 %%%-------------------------- time parameters --------------------------%%%
 time.end = (120-1)*(5/60); %%% how long simulation runs in hours
@@ -11,17 +13,16 @@ time.exp_step = 5/60; %%% how many hours between recording a curve
 
 %%%--------------------------- mesh details ----------------------------%%%
 number_nodes = 100;
-exp_px = [258,273]; %size of experimental window in pixels
+exp_px = [522,491]; %size of experimental window in pixels
 exp_scale = .177; %pixels per micron scale
 
 %%%------------------------- model parameters --------------------------%%%
 growthfunction = 'logistic_masslimited';
 
-nameofsavedfile = strcat('Pos14exp6_',growthfunction);
+nameofsavedfile = strcat('Pos1exp2_',growthfunction);
 
-rng('shuffle')
-numwalkers = 2000 - 1428;
-threshold = 1500;
+numruns = 15;
 
-[paramval,minquant] = myemceeinit(numwalkers,threshold,filestring,...
-    nameofsavedfile,growthfunction,time,number_nodes,exp_px,exp_scale);
+filename_previousrun = 'emceeinit_2000samples_Pos1exp2.mat';
+[models,logP] = myemceerun_par(filename_previousrun,numruns,filestring,growthfunction,...
+    time,number_nodes,exp_px,exp_scale);
